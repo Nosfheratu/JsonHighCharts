9839f905c3ef4303ecb2dc919da1d211082bc23a
Fernando Serapio <fhernandosc@gmail.com>
2015-05-15 12:38:09 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JsonHighCharts::Application.config.secret_token = '581a10a3689ce804f3c8da54b67bb37ae360faf3502055c1f94c3ed930ec2328b1a3e0e46e2e98b0b647c8e247ae6792d1995009ab2dee3ee12375ba9a2b780f'
