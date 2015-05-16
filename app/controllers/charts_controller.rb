a88bdf5480ad91e5c0ebcf8b34bfcd56274735f5
Fernando Serapio <fhernandosc@gmail.com>
2015-05-16 05:28:20 -0500

    display spline chart in the index page retrieving data from data.json file using ajax

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -3,5 +3,10 @@ class ChartsController < ApplicationController
   end
 
   def get_data
+    file_path = Rails.root.join('db', 'data.json')
+
+    @data = File.read(file_path)
+
+    render json: @data
   end
 end
